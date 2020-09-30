540ffc6d14c00e62e53bfed39e66ef403e149d1c
ivakhnenkoalex <you@example.comivakhnenko_alex@bk.ru>
2020-09-30 18:58:31 +0300

    Проверте пж

--- ft_straight_code.py
+++ ft_straight_code.py
@@ -1,6 +1,6 @@
-def additional(a):
+def ft_straight_code(a):
     r = ""
-    if a >= 0:
+    if a > 0:
         while a:
             c = a % 2
             if c == 0:
@@ -8,10 +8,11 @@ def additional(a):
             else:
                 r = "1" + r
             a = a // 2
-            while len(r) < 8:
-                r = "0" + r
-        print(r)
-    else:
+        while len(r) < 8:
+            r = "0" + r
+        return (r)
+    if a < 0:
+        r = ""
         a = -a
         while a:
             c = a % 2
@@ -20,6 +21,11 @@ def additional(a):
             else:
                 r = "1" + r
             a = a // 2
-            while len(r) < 8:
-                r = "0" + r
-        print(r)
+
+        while len(r) < 8:
+            r = "0" + r
+        r = '1' + r[1] + r[2] + r[3] + r[4] + r[5] + r[6] + r[7]
+        return (r)
+    if a == 0:
+        r = '00000000'
+        return (r)
